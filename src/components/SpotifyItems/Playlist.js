ebddf104c2d7c3840dc5b47fd8a1274e0b8c1d48
Unknown <jacky.lim@edu.itescia.fr>
2019-06-13 16:44:47 +0200

    Link on image

--- src/components/SpotifyItems/Playlist.js
+++ src/components/SpotifyItems/Playlist.js
@@ -14,11 +14,10 @@ export default class Playlist extends Component {
           {playlists.map(list =>(
             <div class="playlist__item" key={list.id}>
                 <div class="card">
-                    <img src={list.images[0].url} class="card-img-top" alt=""></img>
+                    <a href={list.external_urls.spotify}><img src={list.images[0].url} class="card-img-top" alt=""></img></a>
                     <div class="card-body">
                     <h5 class="card-title">{list.name}</h5>
                     <p class="card-text">{list.type}</p>
-                    <a href={list.external_urls.spotify}><button class="btn btn-success">CLIC</button></a>
                     {console.log(list.external_urls.spotify)}
                     </div>
                 </div> 
