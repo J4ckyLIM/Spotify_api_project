ba8e07a35aef4eb0474fb9574edd3f29e80d4e89
Unknown <jacky.lim@edu.itescia.fr>
2019-06-14 16:13:39 +0200

    Listing correction

--- src/components/SpotifyItems/Tracks.js
+++ src/components/SpotifyItems/Tracks.js
@@ -16,11 +16,11 @@ export default class Tracks extends Component {
         <table class="table table-dark">
             <thead>
                 <tr>
+                <th scope="col">Album</th>
                 <th scope="col">Titre</th>
                 <th scope="col">Popularity</th>
-                <th scope="col">Album</th>
                 <th scope="col">Artist</th>
-                <th scope="ccl">Play button</th>
+                <th scope="ccl">Play It</th>
                 </tr>
             </thead>
             <tbody>
@@ -29,7 +29,6 @@ export default class Tracks extends Component {
                 <img src={track[0].track.album.images[0].url} alt="img" class="img_tracks"></img>
                 <th scope="row">{track[0].track.name}</th>
                 <td>{track[0].track.popularity}</td>
-                <td>{track[0].track.album.name}</td>
                 <td>{track[0].track.album.artists[0].name}</td>
                 <a target="_blank" href={track[0].track.preview_url}><button class="btn btn-success">PLAYYY</button></a>
                 </tr>
